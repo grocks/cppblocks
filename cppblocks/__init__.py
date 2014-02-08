11496ac1a2925edd03015f473bb5e15c7cc021f8
Sascha Friedmann <geraltrocks@gmail.com>
2014-02-08 21:20:30 +0100

    Add skeleton for input argument checking.

--- cppblocks/__init__.py
+++ cppblocks/__init__.py
@@ -2,5 +2,8 @@
 getDisabledBlocks
 '''
 
+from checkargs import check as checkArgs
+
 def getDisabledBlocks(filepath, analyzeHeaders, includeDirsAngle, includeDirsQuote, initialDefines):
+    checkargs.check(filepath, analyzeHeaders, includeDirsAngle, includeDirsQuote, initialDefines)
     return []
---
+++ cppblocks/checkargs.py
@@ -0,0 +1,25 @@
+'''
+This file contains the code to do some checking on the input arguments
+'''
+
+def check(filepath, analyzeHeaders, includeDirsAngle, includeDirsQuote, initialDefines):
+    checkFilePath(filepath)
+    checkAnalyzeHeaders(analyzeHeaders)
+    checkIncludeDirs(includeDirsAngle)
+    checkIncludeDirs(includeDirsQuote)
+    checkDefines(initialDefines)
+
+def checkFilePath(filepath):
+    pass
+
+def checkAnalyzeHeaders(analyzeHeaders):
+    pass
+
+def checkIncludeDirs(includeDirsAngle):
+    pass
+
+def checkIncludeDirs(includeDirsQuote):
+    pass
+
+def checkDefines(initialDefines):
+    pass
