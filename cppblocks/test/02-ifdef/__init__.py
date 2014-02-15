f6f8277541737a2e691482aef2a3564aaf9941c4
Sascha Friedmann <geraltrocks@gmail.com>
2014-02-15 13:31:04 +0100

    Add test for #ifndef.

--- cppblocks/test/02-ifdef/__init__.py
+++ cppblocks/test/02-ifdef/__init__.py
@@ -3,10 +3,20 @@ testCases = [
             "description" : "Define and ifdef in same file works",
             "expected" : [
                   {
-                      "filepath" : "input/main.c",
+                      "filepath" : "input/01-true-and-false-ifdef.c",
                       "disabledBlocks" : [ (7,3) ]
                   }
                 ],
-            "input" : [ "input/main.c", False, None, None, [] ]
+            "input" : [ "input/01-true-and-false-ifdef.c", False, None, None, [] ]
+        },
+        {
+            "description" : "Define and ifndef in same file works",
+            "expected" : [
+                  {
+                      "filepath" : "input/02-true-and-false-ifndef.c",
+                      "disabledBlocks" : [ (3,3) ]
+                  }
+                ],
+            "input" : [ "input/02-true-and-false-ifndef.c", False, None, None, [] ]
         }
 ]
---
+++ cppblocks/test/02-ifdef/input/02-true-and-false-ifndef.c
@@ -0,0 +1,9 @@
+#define FOOBAR
+
+#ifndef FOOBAR
+int i = 3;
+#endif
+
+#ifndef BAR
+int j = 3;
+#endif
