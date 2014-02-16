b144ec4d2731369fd3cb09584f45152ce87c41b0
Sascha Friedmann <geraltrocks@gmail.com>
2014-02-16 18:47:32 +0100

    Test whitespaces after #.

--- cppblocks/test/03-whitespace/__init__.py
+++ cppblocks/test/03-whitespace/__init__.py
@@ -3,10 +3,20 @@ testCases = [
             "description" : "Test whitespace in front of '#'",
             "expected" : [
                   {
-                      "filepath" : "input/01-nested-ifdefs.c",
+                      "filepath" : "input/01-leading-spaces-in-front-of-sharp.c",
                       "disabledBlocks" : [ (1,7), (13,3) ]
                   }
                 ],
-            "input" : [ "input/01-nested-ifdefs.c", False, None, None, {} ]
+            "input" : [ "input/01-leading-spaces-in-front-of-sharp.c", False, None, None, {} ]
+        },
+        {
+            "description" : "Test whitespace in after '#'",
+            "expected" : [
+                  {
+                      "filepath" : "input/02-leading-spaces-after-sharp.c",
+                      "disabledBlocks" : [ (1,7), (13,3) ]
+                  }
+                ],
+            "input" : [ "input/02-leading-spaces-after-sharp.c", False, None, None, {} ]
         }
 ]
---
+++ cppblocks/test/03-whitespace/input/02-leading-spaces-after-sharp.c
@@ -0,0 +1,28 @@
+#  ifdef FOO
+int a = 1;
+#ifdef BAR
+int b = 1;
+# endif
+int c = 1;
+#endif
+
+#	define FOO
+
+#  ifdef FOO
+int a = 2;
+#ifdef BAR
+int b = 2;
+#                           endif
+int c = 2;
+#endif
+
+#define BAR
+
+#ifdef FOO
+int a = 3;
+#			ifdef BAR
+int b = 3;
+#endif
+int c = 3;
+#endif
+
