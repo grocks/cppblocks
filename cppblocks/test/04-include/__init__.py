00cc1fe89464c9f69f91d7ef4e72af9702ea4667
Sascha Friedmann <geraltrocks@gmail.com>
2014-08-30 13:27:27 +0200

    Adjust test 04-include to new "analyzeHeader" semantic.

--- cppblocks/test/04-include/__init__.py
+++ cppblocks/test/04-include/__init__.py
@@ -2,15 +2,17 @@ testCases = [
         {
             "description" : "Angle include of header with define",
             "expected" : {
-                "input/01-single-angle-include.c" : [ (7,3) ]
+                "input/01-single-angle-include.c" : [ (7,3) ],
+                "input/01-single-define.h": []
             },
-            "input" : [ "input/01-single-angle-include.c", False, [ 'input' ], [], {} ]
+            "input" : [ "input/01-single-angle-include.c", True, [ 'input' ], [], {} ]
         },
         {
             "description" : "Quote include of header in same directory with define",
             "expected" : {
-                "input/02-single-quote-include.c" : [ (7,3) ]
+                "input/02-single-quote-include.c" : [ (7,3) ],
+                "input/01-single-define.h": []
             },
-            "input" : [ "input/02-single-quote-include.c", False, [], [], {} ]
+            "input" : [ "input/02-single-quote-include.c", True, [], [], {} ]
         }
 ]
