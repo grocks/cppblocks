0562aa4cff271cfe982a4af5537ec9bdde50042c
Sascha Friedmann <geraltrocks@gmail.com>
2014-09-01 18:49:34 +0200

    atg.py: add --recursive option.

--- cppblocks/atg.py
+++ cppblocks/atg.py
@@ -5,6 +5,7 @@ For usage information see the --help output.
 '''
 
 import argparse
+import os
 
 from autotestgen import makeCppBlocksTest
 
@@ -22,7 +23,17 @@ parser.add_argument('-I', '--include', metavar="PATH", type=filePath(parser, '--
 parser.add_argument('-o', '--outdir', metavar="DIRPATH", type=filePath(parser, '--outdir'), action='store', default='.', help='The directory to store the test in. (Default: current directory.)')
 parser.add_argument('files', metavar="FILE", type=str, nargs='+', help='The source files to generate tests from.')
 parser.add_argument('-P', '--cpp', metavar="CPP", type=str, action='store', default='cpp', help='The CPP command that serves as reference for the Auto Test Generator. (Default: cpp)')
+parser.add_argument('-r', '--recursive', action='store_true', default=False, help='Perform recursive search for headers in given include directories. (Default: no)')
 
 args = parser.parse_args()
 
-makeCppBlocksTest(outdir=args.outdir, sourceFiles=args.files, includeDirs=args.include, preprocessor=args.cpp)
+includeDirs = args.include
+if args.recursive:
+    # To avoid duplicates in our directory list, we use a set
+    includeDirs = set()
+    for includeDir in args.include:
+        # Collect a list of the directory and all subdirectories using os.walk
+        includeDirs.update([x[0] for x in os.walk(includeDir)])
+    includeDirs = list(includeDirs)
+
+makeCppBlocksTest(outdir=args.outdir, sourceFiles=args.files, includeDirs=includeDirs, preprocessor=args.cpp)
